50a1afe280830fe184ae88a56dfe0f47b292972a
Ackshay <ackshayjain@gmail.com>
2018-08-27 21:14:38 +0530

    heroku

--- cargoclear/settings/production.py
+++ cargoclear/settings/production.py
@@ -117,6 +117,7 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_SECONDS = 1000000
 SECURE_FRAME_DENY = True
 
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
-# DATABASES['default']['CONN_MAX_AGE'] = 500
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
